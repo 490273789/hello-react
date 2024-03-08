ff2fdff8a31a8f3a6bf9e0e6e25216affab39593
王绍楠 <wangsn666@163.com>
2024-03-08 15:41:27 +0800

    feat: space组件开发

---
+++ src/components/Space/index.tsx
@@ -0,0 +1,100 @@
+import {
+  CSSProperties,
+  Fragment,
+  HTMLAttributes,
+  useContext,
+  useMemo
+} from 'react';
+import { FC, Children } from 'react';
+export type SizeType = 'small' | 'middle' | 'large' | number | undefined;
+import cn from 'classnames';
+import './index.scss';
+import { ConfigContext } from './ConfigProvider';
+
+interface SpaceProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+  style?: CSSProperties;
+  size?: SizeType | [SizeType, SizeType];
+  direction?: 'horizontal' | 'vertical';
+  align?: 'start' | 'end' | 'center' | 'baseline';
+  split?: React.ReactNode;
+  wrap?: boolean;
+}
+
+export interface ConfigContextType {
+  space?: {
+    size?: SizeType;
+  };
+}
+
+const spaceSize = {
+  small: 8,
+  middle: 16,
+  large: 24
+};
+const getNumberSize = (size: SizeType) => {
+  return typeof size === 'string' ? spaceSize[size] : size ?? 0;
+};
+
+const Space: FC<SpaceProps> = (props) => {
+  const { space } = useContext(ConfigContext);
+  const {
+    className,
+    style,
+    size = space?.size ?? 'small',
+    direction = 'horizontal',
+    align,
+    split,
+    wrap,
+    ...otherProps
+  } = props;
+
+  const childNodes = Children.toArray(props.children);
+
+  const mergedAlign =
+    direction === 'horizontal' && align === undefined ? 'center' : align;
+
+  const classnames = cn('space', `space-${direction}`, {
+    [`space-align-${mergedAlign}`]: mergedAlign,
+    className
+  });
+
+  const nodes = childNodes.map((child: any, i) => {
+    const key = (child && child.key) ?? `space-item-${i}`;
+    return (
+      <Fragment key={key}>
+        <div className="space-item">{child}</div>
+        {i < childNodes.length - 1 && split && (
+          <span className={`${className}-split`} style={style} key={i}>
+            {split}
+          </span>
+        )}
+      </Fragment>
+    );
+  });
+
+  const otherStyles: CSSProperties = {};
+  const [horizontalSize, verticalSize] = useMemo(
+    () =>
+      (Array.isArray(size) ? size : ([size, size] as [SizeType, SizeType])).map(
+        (item) => getNumberSize(item)
+      ),
+    [size]
+  );
+
+  otherStyles.columnGap = horizontalSize;
+  otherStyles.rowGap = verticalSize;
+  if (wrap) otherStyles.flexWrap = 'wrap';
+
+  return (
+    <div
+      className={classnames}
+      style={{ ...otherStyles, ...style }}
+      {...otherProps}
+    >
+      {nodes}
+    </div>
+  );
+};
+
+export default Space;
--- src/views/uiComponent/index.tsx
+++ src/views/uiComponent/index.tsx
@@ -1,8 +1,11 @@
 import { useRef } from 'react';
 import Calendar from '@components/Calendar';
+import { Space as AntSpace } from 'antd';
 import dayjs from 'dayjs';
 import Button from '@/components/Button';
 import MiniCalendar, { CalendarRef } from '@/components/MiniCalendar';
+import Space from '@/components/Space';
+
 export default function UIComponent() {
   const calendarRef = useRef<CalendarRef>(null);
   const handleCalendarChange = (date: Date) => {
@@ -36,11 +39,38 @@ export default function UIComponent() {
             );
           }} */}
       <div className="p24">
-        <Calendar
+        {/* <Calendar
           value={dayjs('2024-02-18')}
           className={'aaa'}
           locale="en-US"
-        />
+        /> */}
+      </div>
+
+      <div></div>
+
+      <div>
+        <Space size={20} align="end" wrap split={'|'}>
+          <div>123</div>
+          <div>123</div>
+          <div>123</div>
+          <div>123</div>
+          <div>123</div>
+        </Space>
+
+        {/* <AntSpace size={20} align="end" wrap split={'|'}>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+          <Button>设置时间</Button>
+        </AntSpace> */}
       </div>
     </div>
   );
